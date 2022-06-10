16715183a619f805269e7a0d118a67417e84cde0
Francisco Neto <wantedshark@gmail.com>
2022-06-10 14:54:03 -0300

    cakes removed

--- src/components/produtos/BolosJson.js
+++ src/components/produtos/BolosJson.js
@@ -2,23 +2,15 @@ import boloDeAbacaxi from "../../imgs/bolos/abacaxi.jpg"
 import boloDe4Leites from "../../imgs/bolos/4leites.jpg"
 import boloDebanana from "../../imgs/bolos/banana.jpg"
 import boloDebatata from "../../imgs/bolos/batata.jpg"
-import boloDebeijinho from "../../imgs/bolos/beijinho.jpg"
 import boloDebrigadeiro from "../../imgs/bolos/brigadeiro.jpg"
-import boloDeformigueiroDeCenoura from "../../imgs/bolos/cenoura-formigueiro.jpg"
-import boloDeCenouraMesclado from "../../imgs/bolos/cenoura-mesclado.jpg"
 import boloDeCenoura from "../../imgs/bolos/cenoura.jpg"
-import boloDeChoco4ltChocoBranco from "../../imgs/bolos/choco-4leite-choco-branco.jpg"
 import boloDeCocada from "../../imgs/bolos/cocada.jpg"
 import boloDeDoisAmores from "../../imgs/bolos/dois-amores.jpeg"
-import boloFofoDoceDeLeite from "../../imgs/bolos/fofo-doce-de-leite.jpg"
 import boloFofoFormigueiro from "../../imgs/bolos/fofo-formigueiro.jpg"
 import boloFofo from "../../imgs/bolos/fofo.jpg"
 import boloDeFuba from "../../imgs/bolos/fuba.jpg"
-import boloDeFubaComGoiabada from "../../imgs/bolos/fuba-com-goiabada.jpeg"
-import boloMacaDoAmor from "../../imgs/bolos/maça-do-amor.jpeg"
 import boloMoleLeiteDeCoco from "../../imgs/bolos/mole-leite-de-coco.jpeg"
 import boloMoleLeite from "../../imgs/bolos/mole-leite.jpeg"
-import boloDePacoca from "../../imgs/bolos/paçoca.jpg"
 import boloPiscinaComLeiteDeCoco from "../../imgs/bolos/piscina-creme-de-coco.jpg"
 import boloPiscinaDeChurros from "../../imgs/bolos/piscina-churros.jpeg"
 import boloDePrestigio from "../../imgs/bolos/prestigio.jpeg"
@@ -26,124 +18,84 @@ import boloDePrestigio from "../../imgs/bolos/prestigio.jpeg"
 
 export const BolosJson = [
     {
-        name:"bolo de abacaxi",
-        img:boloDeAbacaxi,
-        price:18
+        name: "bolo de abacaxi",
+        img: boloDeAbacaxi,
+        price: 18
     },
     {
-        name:"bolo 4 leite",
-        img:boloDe4Leites,
-        price:20
+        name: "bolo 4 leite",
+        img: boloDe4Leites,
+        price: 20
     },
     {
-        name:"bolo de banana",
-        img:boloDebanana,
-        price:17
+        name: "bolo de banana",
+        img: boloDebanana,
+        price: 17
     },
     {
-        name:"bolo de batata",
-        img:boloDebatata,
-        price:20
+        name: "bolo de batata",
+        img: boloDebatata,
+        price: 20
     },
     {
-        name:"bolo de beijinho",
-        img:boloDebeijinho,
-        price:18
+        name: "bolo de brigadeiro",
+        img: boloDebrigadeiro,
+        price: 20
     },
     {
-        name:"bolo de brigadeiro",
-        img:boloDebrigadeiro,
-        price:20
+        name: "bolo de cenoura com calda",
+        img: boloDeCenoura,
+        price: 20
     },
     {
-        name:"bolo de cenoura formigueiro",
-        img:boloDeformigueiroDeCenoura,
-        price:16
+        name: "bolo de cocada",
+        img: boloDeCocada,
+        price: 18
     },
     {
-        name:"bolo de cenoura mesclado",
-        img:boloDeCenouraMesclado,
-        price:16
+        name: "bolo dois amores",
+        img: boloDeDoisAmores,
+        price: 20
     },
     {
-        name:"bolo de cenoura com calda",
-        img:boloDeCenoura,
-        price:20
+        name: "bolo fofo formigueiro",
+        img: boloFofoFormigueiro,
+        price: 16
     },
     {
-        name:"bolo de chocolate com 4 leite",
-        img:boloDeChoco4ltChocoBranco,
-        price:20
+        name: "bolo fofo",
+        img: boloFofo,
+        price: 14
     },
     {
-        name:"bolo de cocada",
-        img:boloDeCocada,
-        price:18
+        name: "bolo de fubá",
+        img: boloDeFuba,
+        price: 14
     },
     {
-        name:"bolo dois amores",
-        img:boloDeDoisAmores,
-        price:20
+        name: "bolo mole de leite de coco",
+        img: boloMoleLeiteDeCoco,
+        price: 20
     },
     {
-        name:"bolo fofo de doce de leite",
-        img:boloFofoDoceDeLeite,
-        price:20
+        name: "bolo mole de leite",
+        img: boloMoleLeite,
+        price: 20
     },
     {
-        name:"bolo fofo formigueiro",
-        img:boloFofoFormigueiro,
-        price:16
+        name: "bolo piscina com creme de coco",
+        img: boloPiscinaComLeiteDeCoco,
+        price: 22
     },
     {
-        name:"bolo fofo",
-        img:boloFofo,
-        price:14
+        name: "bolo piscina de churros",
+        img: boloPiscinaDeChurros,
+        price: 22
     },
     {
-        name:"bolo de fubá",
-        img:boloDeFuba,
-        price:14
-    }, 
-    {
-        name:"bolo de fubá com goiabada",
-        img:boloDeFubaComGoiabada,
-        price:18
+        name: "bolo prestigio",
+        img: boloDePrestigio,
+        price: 22
     },
-    {
-        name:"bolo maça do amor",
-        img:boloMacaDoAmor,
-        price:17
-    },
-    {
-        name:"bolo mole de leite de coco",
-        img:boloMoleLeiteDeCoco,
-        price:20
-    },
-    {
-        name:"bolo mole de leite",
-        img:boloMoleLeite,
-        price:20
-    },
-    {
-        name:"bolo de paçoca",
-        img:boloDePacoca,
-        price:16
-    },
-    {
-        name:"bolo piscina com creme de coco",
-        img:boloPiscinaComLeiteDeCoco,
-        price:22
-    },
-    {
-        name:"bolo piscina de churros",
-        img:boloPiscinaDeChurros,
-        price:22
-    },
-    {
-        name:"bolo prestigio",
-        img:boloDePrestigio,
-        price:22
-    },
-    
+
 ]
